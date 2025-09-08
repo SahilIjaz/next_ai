7e3643d84a79b4629140d91c7a666e047289be89
SahilIjaz <hssahil2913@gmail.com>
2025-09-08 11:26:02 +0500

    full stack with ai created

---
+++ app/api/chat/route.ts
@@ -0,0 +1,28 @@
+import OpenAI from "openai";
+
+const openAi = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(requuest) {
+  try {
+    const { message } = await requuest.json();
+
+    const response = await openAi.chat.completions.create({
+      model: "gpt-3.5-turbo-0125",
+      messages: [{ role: "user", content: message }],
+    });
+
+    return Response.json({
+      message: response.choices[0].message.content,
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        message: "failed to fetch response from openai",
+        error: true,
+      },
+      { status: 4000 }
+    );
+  }
+}
